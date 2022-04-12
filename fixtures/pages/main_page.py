4dfdcec82bd576e3a467535c6df71b933318bec8
alexanderlozovoy <berpress@gmail.com>
2022-04-12 20:05:34 +0300

    Add base page

--- fixtures/pages/main_page.py
+++ fixtures/pages/main_page.py
@@ -1,30 +1,26 @@
 from selenium.webdriver.common.by import By
 
+from fixtures.pages.base_page import BasePage
 
-class MainPage:
+
+class MainPage(BasePage):
     INPUT = (By.ID, "input")
     SUBMIT = (By.ID, "submit")
     ERROR_TEXT = (By.CSS_SELECTOR, ".feedback")
-
-    def __init__(self, app):
-        self.app = app
 
     def open_main_page(self):
         """
         open main page
         """
-        self.app.driver.get(self.app.url)
+        self.open_page(self.app.url)
 
     def add_css_url(self, url: str):
         """
         Add css url on main page
         :param url: url
         """
-        input_field = self.app.driver.find_element(*self.INPUT)
-        input_field.send_keys(url)
-        start_button = self.app.driver.find_element(*self.SUBMIT)
-        start_button.click()
+        self.fill(locator=self.INPUT, value=url)
+        self.click(locator=self.SUBMIT)
 
     def error_text(self) -> str:
-        element = self.app.driver.find_element(*self.ERROR_TEXT)
-        return element.text
+        return self.text(locator=self.ERROR_TEXT)
